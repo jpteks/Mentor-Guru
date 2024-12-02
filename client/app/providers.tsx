62d2dd936b00084cd9a3b94f42328a0da9dc6b55
651118070 <valentinpola7@gmail.com>
2024-12-02 04:32:21 +0100

    backend limk modified

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -1,26 +1,4 @@
 // app/providers.tsx
-<<<<<<< HEAD
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
-
-export function PHProvider({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-    useEffect(() => {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        person_profiles: 'identified_only',
-        capture_pageview: false // Disable automatic pageview capture, as we capture manually
-      })
-  }, []);
-
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
-}
-=======
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
@@ -37,4 +15,3 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
 
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
->>>>>>> 0a75d5028136a5b6f3f5e00e9cd8af618dcce4b7
